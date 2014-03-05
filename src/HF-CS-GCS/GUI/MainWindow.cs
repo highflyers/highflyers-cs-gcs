9552bd0b10c0c5283c98a19bbae52de6e852ec3d
loganek <marcin.kolny@gmail.com>
2014-03-05 18:31:22 +0100

    adedded project files

---
+++ src/HF-CS-GCS/GUI/MainWindow.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace HF_CS_GCS
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
