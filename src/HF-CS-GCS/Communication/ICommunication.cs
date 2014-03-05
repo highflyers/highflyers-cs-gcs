60ccae0938f3cbba0970caf851a8e4331a7bb9ec
loganek <marcin.kolny@gmail.com>
2014-03-05 18:57:49 +0100

    added communication interface

---
+++ src/HF-CS-GCS/Communication/ICommunication.cs
@@ -0,0 +1,13 @@
+﻿namespace HF_CS_GCS.Communication
+{
+    delegate void DataReceivedEventHandler(byte[] data); 
+
+    interface ICommunication
+    {
+        int SendData(byte[] data);
+        void Open();
+        void Close();
+        bool IsOpen { get; set; }
+        event DataReceivedEventHandler DataReceived;
+    }
+}
