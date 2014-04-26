6de9b5079871a756bedee3f8fe85c04085130a96
Marcin Kolny <marcin.kolny@gmail.com>
2014-04-26 03:01:22 +0200

    added first button icon, minors in gui

--- src/HighFlyersCsGCS/MainWindow.cs
+++ src/HighFlyersCsGCS/MainWindow.cs
@@ -19,6 +19,8 @@ namespace HighFlyers.GCS
 			startStopCameraToggleButton.Toggled += HandleStartStopClicked;
 
 			video = new VideoWidget ();
+			video.Expand = true;
+
 			box2.Add (video);
 			video.Show ();
 
