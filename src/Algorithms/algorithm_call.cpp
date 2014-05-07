155cc83fe67db5f65ba41990b26b7d31c65565e3
Marcin Kolny <marcin.kolny@gmail.com>
2014-05-07 19:18:34 +0200

    added sample code for processing image

--- src/Algorithms/algorithm_call.cpp
+++ src/Algorithms/algorithm_call.cpp
@@ -4,6 +4,15 @@ extern "C" {
 
 int dummy_method (GstBuffer* buf)
 {
+	GstMapInfo map;
+
+	if (gst_buffer_map (buf, &map, GST_MAP_READ))
+	{	
+		// algorithm code here
+		// usage: map.data <- raw data		
+  		gst_buffer_unmap (buf, &map);
+	}
+	
 	return gst_buffer_get_size(buf);
 }
 
